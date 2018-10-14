19cf805ced34501049569cbebcfe36f2e68e75ce
Kylir <jason.forest@glyphe.org>
2018-10-15 00:42:19 +0100

    more play. change direction.

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const ain1 = new Gpio(17, {mode: Gpio.OUTPUT})
 const ain2 = new Gpio(27, {mode: Gpio.OUTPUT})
 const pwma = new Gpio(22, {mode: Gpio.OUTPUT})
 
-// Set ain1 to ON and ain2 to OFF 
-ain1.digitalWrite(1)
-ain2.digitalWrite(0)
+let isClockwise = true
 
 // Vary the PWM from 0 to 255 every 20 ms
 let dutyCycle = 0
-setInterval(() => {
+let interval = setInterval(() => {
+  
+  ain1.digitalWrite(direction ? 1 : 0)
+  ain2.digitalWrite(direction ? 0 : 1)
+
   pwma.pwmWrite(dutyCycle)
   dutyCycle += 5
   if (dutyCycle > 255) {
     dutyCycle = 0
+    isClockwise = !isClockwise
   }
-}, 20)
+}, 200)
+
 
 
