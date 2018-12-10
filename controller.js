89fcca4a9ff859ce2fb9b5ea48f58d368c7ced70
Kylir <jason.forest@glyphe.org>
2018-12-10 01:20:52 +0000

    change controller library

--- controller.js
+++ controller.js
@@ -1,54 +1,20 @@
-const ds = require('dualshock')
+var dualShock = require('dualshock-controller')
+ 
+var controller = dualShock({
+    config: "dualshock4-generic-driver",
+})
 
-let list = ds.getDevices()
-let device = list[0]
-let gamepad = ds.open(device, {smoothAnalog:10, smoothMotion:15, joyDeadband:4, moveDeadband:4})
+controller.on("left:move", data => {
+    console.log(`Left move received: ( ${x} , ${y} )`)
+})
+controller.on("right:move", data => {
+    console.log(`Right move received: ( ${x} , ${y} )`)
+})
 
-gamepad.onmotion = true
-gamepad.onstatus = true
+controller.on('square:press', ()=> {
+    console.log('square press')
+})
 
-//DS4 Only: Random LED Stuffs:
-/*setInterval(function() {
-    gamepad.setLed(Math.floor(Math.random()*255),Math.floor(Math.random()*255),Math.floor(Math.random()*255));
-}, 100)*/
-
-gamepad.onupdate = function(changed) {
-    //rumbleScript(changed, this);
-    //Uncomment one of these lines for debugging!
-    
-    console.log(changed);
-    
-    //console.log(this.digital);
-    //console.log(this.analog);
-    //console.log(this.motion,this.status);
-}
-
-/*
-function rumbleScript(chg, g) {
-    //Rumble On:
-    if (chg.l2 || chg.r2) {
-        g.rumbleAdd(g.analog.l2?g.analog.l2:-1, g.analog.r2?255:-1, 254, 254);
-        console.log("rumble set", [g.analog.l2,(g.analog.r2>0)?255:0]); 
-    } else if (chg.l3 && g.digital.l3) {
-        g.rumbleAdd(94, 0, 255, 0);
-        console.log("rumble slow");
-    } else if(chg.start && g.digital.start) {
-        g.rumbleAdd(0, 255, 0, 5);
-        console.log("rumble tap"); 
-    }
-    
-    //Rumble Off:
-    if( (chg.l2 || chg.r2 || chg.l3 || chg.start) && !(g.analog.l2 || g.analog.r2 || g.digital.l3 || g.digital.start)) {
-        g.rumble(0, 0); console.log("rumble off");
-    }
-    
-    //Change LED Pattern:
-    if(chg.ps && g.digital.ps) {
-        g.setLed(nLedVal);
-        console.log("led set "+nLedVal); nLedVal++;
-        if(nLedVal > 15) {
-            nLedVal = 0; 
-        }
-    }
-}
-*/
+controller.on('square:release', () => {
+    console.log('square release')
+})
--- readme.md
+++ readme.md
@@ -15,5 +15,22 @@ The application will use the NodeJS module [`pigpio`](https://www.npmjs.com/pack
 
 First goal is to make the robot move!
 
+## Control
+
+To control the robot I decided to use a Playstation Dualshock 4 (DS4) controller.
+It is a bit pricey, but really nice (and I found a way to use it to play games on my PC... another story.)
+
+To interface the controller with the robot I'm going to use the Bluetooth capabilities of the pi.
+
+I used [this tutorial](https://pimylifeup.com/raspberry-pi-playstation-controllers/) to pair the controller with the raspberry.
+
+Also, there are several Node modules to interface with a DS4 controller.
+I decided to give a go at the first one on the list: [dualshock](https://github.com/Pecacheu/dualshock).
+The install of the latest version failed because it appears to be Windows specific (something to do with the sound handling...)
+The version 3.1.1 is working though.
+
+I had to find how to add udev rules so that any user can read the HID data.
+This other Node ([dualshock-controller](https://www.npmjs.com/package/dualshock-controller#create-udev-rules)) module has nice and clear explanations about how to do that.
+
 
 
